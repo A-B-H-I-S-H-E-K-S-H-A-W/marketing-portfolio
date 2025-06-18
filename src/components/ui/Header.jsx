d8fed64c27ca9552cedef898007715a28de84af4
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-06-18 18:56:27 +0530

    Initialized hero section and navbar of home page

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,120 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import { Button } from "./Button";
+import { Menu, X } from "lucide-react";
+import lightLogo from "@/assets/darkLogo.png";
+import darkLogo from "@/assets/lightLogo.png";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+const menuItems = [
+  { name: "Features", href: "#" },
+  { name: "Solution", href: "#" },
+  { name: "Pricing", href: "#" },
+  { name: "About", href: "#" },
+];
+
+const Header = () => {
+  const [menuState, setMenuState] = useState(false);
+  return (
+    <header>
+      <nav
+        data-state={menuState && "active"}
+        className="group fixed z-20 w-full border-b border-dashed bg-white backdrop-blur md:relative dark:bg-zinc-950/50 lg:dark:bg-transparent"
+      >
+        <div className="m-auto max-w-7xl px-6">
+          <div className="flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
+            <div className="flex w-full justify-between lg:w-auto">
+              <Link
+                href="/"
+                aria-label="home"
+                className="flex items-center space-x-2"
+              >
+                <Image
+                  className="hidden dark:block"
+                  src={lightLogo}
+                  alt="app illustration"
+                  width={170}
+                  height={80}
+                />
+                <Image
+                  className="dark:hidden"
+                  src={darkLogo}
+                  alt="app illustration"
+                  width={170}
+                  height={80}
+                />
+              </Link>
+
+              <button
+                onClick={() => setMenuState(!menuState)}
+                aria-label={menuState ? "Close Menu" : "Open Menu"}
+                className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
+              >
+                <Menu className="group-data-[state=active]:rotate-180 group-data-[state=active]:scale-0 group-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
+                <X className="group-data-[state=active]:rotate-0 group-data-[state=active]:scale-100 group-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
+              </button>
+            </div>
+
+            <div className="bg-background group-data-[state=active]:block lg:group-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
+              <div className="lg:pr-4">
+                <ul className="space-y-6 text-base lg:flex lg:gap-8 lg:space-y-0 lg:text-sm">
+                  {menuItems.map((item, index) => (
+                    <li key={index}>
+                      <Link
+                        href={item.href}
+                        className="text-muted-foreground hover:text-accent-foreground block duration-150"
+                      >
+                        <span>{item.name}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit lg:border-l lg:pl-6">
+                <Button variant="outline" size="sm">
+                  <Link href="#">
+                    <span>Contact Us</span>
+                  </Link>
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+function Logo({ className }) {
+  return (
+    <svg
+      viewBox="0 0 78 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn("h-5 w-auto", className)}
+    >
+      <path
+        d="M3 0H5V18H3V0ZM13 0H15V18H13V0ZM18 3V5H0V3H18ZM0 15V13H18V15H0Z"
+        fill="url(#logo-gradient)"
+      />
+      {/* Truncated for brevity */}
+      <defs>
+        <linearGradient
+          id="logo-gradient"
+          x1="10"
+          y1="0"
+          x2="10"
+          y2="20"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#9B99FE" />
+          <stop offset="1" stopColor="#2BC8B7" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default Header;
